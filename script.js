cd1a999647759dab7f37a5f5472a694c8760f66b
Arekuso-kun <alexodrino@gmail.com>
2023-01-08 01:55:38 +0200

    data change

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-for (let i = 0; i < (screen.width/40)*(screen.height/40); i++) {
+for (let i = 0; i < (screen.width/40 +2)*(screen.height/40 +2); i++) { // (screen.width/40)*(screen.height/40)
     const small_square = document.createElement('div');
     const container = document.querySelector('.container');
     small_square.classList.add('small-square');
-    small_square.style.backgroundImage = `url('data/image (${Math.floor(Math.random() * 31) + 1}) (Custom).jpg')`; 
+    small_square.style.backgroundImage = `url('data/image (${Math.floor(Math.random() * 79) + 1}).png')`; 
+    console.log(small_square);
     console.log(Math.floor(Math.random() * 10) + 1);
     container.append(small_square);
 }
